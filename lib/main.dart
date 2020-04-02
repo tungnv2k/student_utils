ae7535b96d7547ad68cf217e482aa732a3038597
Nguyễn Văn Tùng <tungnv@eway.vn>
2020-04-02 19:05:51 +0700

    [Login] Update main for shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:student_utils_app/screens/login_screen.dart';
+import 'package:student_utils_app/screens/login_success_screen.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: LoginScreen(),
-    );
-  }
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  var email = prefs.getString('email');
+  runApp(
+      MaterialApp(home: email == null ? LoginScreen() : LoginSuccessScreen()));
 }
