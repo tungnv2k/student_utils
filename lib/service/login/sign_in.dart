eed6cf7cd946cc9b291d7cb98e6f25fd8bdcd228
Nguyễn Văn Tùng <tungnv@eway.vn>
2020-04-08 00:11:30 +0700

    [Login] Sign in automatically as last user

--- lib/service/login/sign_in.dart
+++ lib/service/login/sign_in.dart
@@ -1,44 +1,62 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:student_utils_app/screens/login_success_screen.dart';
+import 'package:student_utils_app/service/googleapi/apis.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
-final GoogleSignIn googleSignIn = GoogleSignIn();
+final GoogleSignIn googleSignIn = GoogleSignIn(
+  scopes: [
+    'email',
+    'https://www.googleapis.com/auth/calendar',
+    'https://www.googleapis.com/auth/calendar.events',
+  ],
+);
 
-bool isLoggedIn;
 String name;
 String email;
 String imageUrl;
 
-Future<String> signInWithGoogle() async {
-  final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+Future<bool> signInWithGoogle({bool silently = false}) async {
+  GoogleSignInAccount googleSignInAccount;
+  FirebaseUser user;
 
-  var status = await googleSignIn.isSignedIn();
-  isLoggedIn = status;
+  try {
+    if (silently) {
+      googleSignInAccount = await googleSignIn.signInSilently();
+    } else {
+      googleSignInAccount = await googleSignIn.signIn();
 
-  final GoogleSignInAuthentication googleSignInAuthentication =
-      await googleSignInAccount.authentication;
+      GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount.authentication;
 
-  final AuthCredential credential = GoogleAuthProvider.getCredential(
-    accessToken: googleSignInAuthentication.accessToken,
-    idToken: googleSignInAuthentication.idToken,
-  );
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
 
-  final FirebaseUser user = await _auth.signInWithCredential(credential);
+      user = await _auth.signInWithCredential(credential);
 
-  assert(!user.isAnonymous);
-  assert(await user.getIdToken() != null);
+      assert(!user.isAnonymous);
+      assert(await user.getIdToken() != null);
 
-  final FirebaseUser currentUser = await _auth.currentUser();
-  assert(user.uid == currentUser.uid);
+      final FirebaseUser currentUser = await _auth.currentUser();
+      assert(user.uid == currentUser.uid);
 
-  assert(user.email != null);
-  assert(user.displayName != null);
-  assert(user.photoUrl != null);
-  name = user.displayName;
-  email = user.email;
-  imageUrl = user.photoUrl;
+      assert(user.email != null);
+      assert(user.displayName != null);
+      assert(user.photoUrl != null);
+      name = user.displayName;
+      email = user.email;
+      imageUrl = user.photoUrl;
+    }
+    final authHeaders = await googleSignIn.currentUser.authHeaders;
+    getApiAccess(authHeaders);
+  } catch (err) {
+    print(err);
+  }
 
-  return 'signInWithGoogle succeeded: $user';
+  return user != null;
 }
 
 void signOutGoogle() async {
@@ -46,4 +64,6 @@ void signOutGoogle() async {
 
   SharedPreferences prefs = await SharedPreferences.getInstance();
   prefs.remove('email');
+
+  dateEvents.clear();
 }
