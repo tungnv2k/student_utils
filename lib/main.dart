b79fb86ea6830b46a67492d5e0dbe281503f8e8e
Nguyễn Văn Tùng <tungnv@eway.vn>
2020-04-08 00:17:56 +0700

    [Theme] Add font and route

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:student_utils_app/screens/bookmark_screen.dart';
+import 'package:student_utils_app/screens/calendar_screen.dart';
 import 'package:student_utils_app/screens/login_screen.dart';
 import 'package:student_utils_app/screens/login_success_screen.dart';
+import 'package:student_utils_app/screens/note_list_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences prefs = await SharedPreferences.getInstance();
   var email = prefs.getString('email');
-  runApp(
-      MaterialApp(home: email == null ? LoginScreen() : LoginSuccessScreen()));
+  runApp(MaterialApp(
+    theme: ThemeData(fontFamily: 'ProximaNovaSoft'),
+    home: email == null ? LoginScreen() : LoginSuccessScreen(),
+    routes: {
+      '/login': (context) => LoginScreen(),
+      '/login_success': (context) => LoginSuccessScreen(),
+      '/calendar': (context) => CalendarScreen(),
+      '/note_list': (context) => NoteListScreen(),
+      '/bookmark': (context) => BookmarkScreen(),
+    },
+  ));
 }
