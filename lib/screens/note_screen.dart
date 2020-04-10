736d1b1b1c1a752e9dfdfb046019097d1aef93e2
Nguyễn Văn Tùng <tungnv@eway.vn>
2020-04-10 13:14:21 +0700

    Hotfix #32: Add back button to pop back from note screen

--- lib/screens/note_screen.dart
+++ lib/screens/note_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show LengthLimitingTextInputFormatter;
 import 'package:student_utils_app/components/app_bar.dart';
 import 'package:student_utils_app/models/note.dart';
-import 'package:student_utils_app/screens/note_list_screen.dart';
 
 class NoteScreen extends StatefulWidget {
   final Note note;
@@ -37,66 +36,59 @@ class NoteScreenState extends State<NoteScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      child: Scaffold(
-        body: Container(
-            padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-            child: Stack(children: <Widget>[
-              SingleChildScrollView(
-                controller: _scrollController,
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(height: 60.0),
-                    Container(
-                      color: Colors.amberAccent,
-                      margin: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 0.0),
-                      padding: EdgeInsets.fromLTRB(25.0, 5.0, 25.0, 0.0),
-                      child: Column(
-                        children: <Widget>[
-                          TextField(
-                            controller: _titleTextController,
-                            inputFormatters: [
-                              LengthLimitingTextInputFormatter(15)
-                            ],
-                            style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.w600,
-                                color: Colors.black.withOpacity(0.80)),
-                            decoration: InputDecoration.collapsed(
-                              hintText: "Give this note a title...",
-                              hintStyle: TextStyle(color: Colors.black45),
-                            ),
+    return Scaffold(
+      body: Container(
+          padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+          child: Stack(children: <Widget>[
+            SingleChildScrollView(
+              controller: _scrollController,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 60.0),
+                  Container(
+                    color: Colors.amberAccent,
+                    margin: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 0.0),
+                    padding: EdgeInsets.fromLTRB(25.0, 5.0, 25.0, 0.0),
+                    child: Column(
+                      children: <Widget>[
+                        TextField(
+                          controller: _titleTextController,
+                          inputFormatters: [
+                            LengthLimitingTextInputFormatter(15)
+                          ],
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black.withOpacity(0.80)),
+                          decoration: InputDecoration.collapsed(
+                            hintText: "Give this note a title...",
+                            hintStyle: TextStyle(color: Colors.black45),
                           ),
-                          Divider(height: 5.0),
-                          SizedBox(height: 10.0),
-                          TextField(
-                            controller: _textController,
-                            maxLines: null,
-                            style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.w400,
-                                color: Colors.black.withOpacity(0.80)),
-                            decoration: InputDecoration.collapsed(
-                              hintText: "Write something...",
-                              hintStyle: TextStyle(color: Colors.black45),
-                            ),
+                        ),
+                        Divider(height: 5.0),
+                        SizedBox(height: 10.0),
+                        TextField(
+                          controller: _textController,
+                          maxLines: null,
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.black.withOpacity(0.80)),
+                          decoration: InputDecoration.collapsed(
+                            hintText: "Write something...",
+                            hintStyle: TextStyle(color: Colors.black45),
                           ),
-                          SizedBox(height: 200.0)
-                        ],
-                      ),
+                        ),
+                        SizedBox(height: 200.0)
+                      ],
                     ),
-                    SizedBox(height: MediaQuery.of(context).size.height)
-                  ],
-                ),
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height)
+                ],
               ),
-              buildTopBar(title = widget.note.title),
-            ])),
-      ),
-      onWillPop: () => Future.value(Navigator.of(context).pushAndRemoveUntil(
-          MaterialPageRoute(
-            builder: (context) => NoteListScreen(),
-          ),
-          ModalRoute.withName("/note_list"))),
+            ),
+            buildSubTopBar(title = widget.note.title, context),
+          ])),
     );
   }
 }
