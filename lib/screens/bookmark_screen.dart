069eaedc91accd04a35bf708beaf4922f37d7278
Nguyễn Văn Tùng <thong1234das@yahoo.com.vn>
2020-04-12 18:09:17 +0700

    [Bookmark] Input bookmark

--- lib/screens/bookmark_screen.dart
+++ lib/screens/bookmark_screen.dart
@@ -1,81 +1,78 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:student_utils_app/models/bookmark.dart';
 
 class BookmarkScreen extends StatefulWidget {
+  final Bookmark bookmark;
+
+  BookmarkScreen({Key key, this.bookmark}) : super(key: key);
+
   @override
   State createState() => BookmarkScreenState();
 }
 
 class BookmarkScreenState extends State<BookmarkScreen> {
+  TextEditingController _titleController;
+  TextEditingController _linkController;
+
+  @override
+  void initState() {
+    super.initState();
+    _titleController = TextEditingController(text: widget.bookmark.title);
+    _linkController = TextEditingController(text: widget.bookmark.link);
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _linkController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Center(child: Text("Bookmarks"))),
-      body: Container(
+      body: Column(
+        children: <Widget>[
+          SizedBox(height: 100.0),
+          Container(
+            padding: const EdgeInsets.all(30.0),
+            child: TextField(
+              controller: _titleController,
+              maxLines: 1,
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
+              decoration: InputDecoration.collapsed(
+                  hintText: "Enter title...",
+                  hintStyle: TextStyle(color: Colors.black45)),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(30.0),
+            child: TextField(
+              controller: _linkController,
+              maxLines: 1,
+              style: TextStyle(fontSize: 15.0, color: Colors.grey[800]),
+              decoration: InputDecoration.collapsed(
+                  hintText: "Enter link...",
+                  hintStyle: TextStyle(color: Colors.black38)),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: Padding(
         padding: EdgeInsets.all(8.0),
-        color: Color.fromRGBO(238, 238, 238, 1.0),
-        child: Column(
-          children: <Widget>[
-            _buildBookmark("SE 2020", "http://link.to.se.excel"),
-            _buildBookmark("OS 2020", "https://link.to.check.attendance"),
-            _buildBookmark("OOP BI9-229", "http://google.drive.oop.com"),
-          ],
+        child: FloatingActionButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Icon(
+            EvaIcons.close,
+            size: 35.0,
+            color: Colors.blue,
+          ),
+          backgroundColor: Colors.white,
         ),
       ),
     );
-  }
-
-  Widget _buildBookmark(String name, String link) {
-    return Container(
-        padding: EdgeInsets.fromLTRB(20.0, 8.0, 20.0, 8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.all(8.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(15.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: Color.fromRGBO(0, 0, 0, 0.08),
-                    blurRadius: 4,
-                    offset: Offset(0, 4),
-                  )
-                ],
-              ),
-              child: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.all(7.0),
-                    child: CircleAvatar(
-                      child: Icon(Icons.event),
-                    ),
-                  ),
-                  Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            name,
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            link,
-                            style: TextStyle(
-                              fontSize: 15,
-                              color: Colors.grey,
-                            ),
-                          )
-                        ],
-                      ))
-                ],
-              ),
-            ),
-          ],
-        ));
   }
 }
