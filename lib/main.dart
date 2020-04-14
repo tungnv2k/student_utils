30d51291586489b783da1b6f17164cb8053e00f0
Nguyễn Văn Tùng <tungnv@eway.vn>
2020-04-14 21:56:11 +0700

    [Theme] Remove routes and fontFamily

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:student_utils_app/screens/bookmark_screen.dart';
-import 'package:student_utils_app/screens/calendar_screen.dart';
 import 'package:student_utils_app/screens/login_screen.dart';
-import 'package:student_utils_app/screens/login_success_screen.dart';
-import 'package:student_utils_app/screens/note_list_screen.dart';
 import 'package:student_utils_app/screens/no_login_screen.dart';
 
 Future<void> main() async {
@@ -12,15 +8,7 @@ Future<void> main() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   var email = prefs.getString('email');
   runApp(MaterialApp(
-    theme: ThemeData(fontFamily: 'ProximaNovaSoft'),
-    home: email == null ? LoginScreen() : LoginSuccessScreen(),
-    routes: {
-      '/login': (context) => LoginScreen(),
-      '/login_success': (context) => LoginSuccessScreen(),
-      '/calendar': (context) => CalendarScreen(),
-      '/note_list': (context) => NoteListScreen(),
-      '/bookmark': (context) => BookmarkScreen(),
-    },
+    theme: ThemeData(),
     home: email == null ? LoginScreen() : NoLoginScreen(),
   ));
 }
