e32e55a8d637254aa235ab520f68b4892fb23cac
Nguyễn Văn Tùng <tungnv@eway.vn>
2020-04-21 20:10:51 +0700

    [AppBar] Refactor for PageView app bar

--- lib/components/app_bar.dart
+++ lib/components/app_bar.dart
@@ -1,58 +1,56 @@
-import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 
-Widget buildTopBar(String title,
-    {Color color = Colors.black, Color titleColor = Colors.white}) {
-  return Container(
-    color: color,
-    height: 60.0,
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        Container(
-            padding: EdgeInsets.only(left: 15.0),
-            alignment: Alignment.centerLeft,
-            child: Icon(EvaIcons.menu, color: Colors.white, size: 25)),
-        Container(
-          alignment: Alignment.center,
-          child: Text(
-            title,
-            style: TextStyle(
-                color: titleColor, fontSize: 20.0, fontWeight: FontWeight.w600),
-          ),
+Widget buildTopBar(
+    {String title,
+    IconData leftIcon,
+    IconData rightIcon,
+    Color color = Colors.black,
+    Color titleColor = Colors.white,
+    Function onTitleTap,
+    Function onLeftTap,
+    Function onRightTap}) {
+  return PreferredSize(
+    child: SafeArea(
+      child: Container(
+        color: color,
+        height: 60.0,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+                alignment: Alignment.centerLeft,
+                child: IconButton(
+                  icon: Icon(leftIcon, color: Colors.white, size: 20),
+                  splashColor: Colors.white,
+                  onPressed: onLeftTap,
+                )),
+            Expanded(
+              child: GestureDetector(
+                child: Container(
+                  alignment: Alignment.center,
+                  child: Text(
+                    title,
+                    style: TextStyle(
+                        color: titleColor,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ),
+                onTap: onTitleTap,
+              ),
+            ),
+            Container(
+                alignment: Alignment.centerRight,
+                child: IconButton(
+                  icon: Icon(rightIcon,
+                      color: Colors.white, size: 20),
+                  splashColor: Colors.white,
+                  onPressed: onRightTap,
+                )),
+          ],
         ),
-        Container(
-            padding: EdgeInsets.only(right: 15.0),
-            alignment: Alignment.centerRight,
-            child: Icon(EvaIcons.moreVertical, color: Colors.white, size: 20)),
-      ],
+      ),
     ),
-  );
-}
-
-Widget buildSubTopBar(String title, BuildContext context) {
-  return Container(
-    color: Colors.black,
-    padding: EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 15.0),
-    child: Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        GestureDetector(
-          child: Container(
-              width: 30.0,
-              child: Icon(EvaIcons.arrowBack, color: Colors.white, size: 20)),
-          onTap: () => Navigator.of(context).pop(),
-        ),
-        Text(
-          title,
-          style: TextStyle(
-              color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.w600),
-        ),
-        Container(
-            width: 30.0,
-            child: Icon(EvaIcons.moreVertical, color: Colors.white, size: 20)),
-      ],
-    ),
+    preferredSize: Size(double.infinity, 60.0),
   );
 }
