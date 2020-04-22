3bedd6be39b7aa55e5374e56f589136a2c8e2a8e
Nguyễn Văn Tùng <thong1234das@yahoo.com.vn>
2020-04-22 17:07:57 +0700

    [Note-taking] Fix save Note edit

--- lib/screens/note_screen.dart
+++ lib/screens/note_screen.dart
@@ -1,3 +1,4 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show LengthLimitingTextInputFormatter;
 import 'package:student_utils_app/components/app_bar.dart';
@@ -37,58 +38,64 @@ class NoteScreenState extends State<NoteScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: buildTopBar(
+          title: title = widget.note.title,
+          leftIcon: EvaIcons.arrowBack,
+          rightIcon: EvaIcons.moreVertical,
+          onLeftTap: () {
+            Navigator.pop(
+              context,
+              Note(title: _titleTextController.text, description: _textController.text),
+            );
+          }),
       body: Container(
-          padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-          child: Stack(children: <Widget>[
-            SingleChildScrollView(
-              controller: _scrollController,
-              child: Column(
-                children: <Widget>[
-                  SizedBox(height: 60.0),
-                  Container(
-                    color: Colors.amberAccent,
-                    margin: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 0.0),
-                    padding: EdgeInsets.fromLTRB(25.0, 5.0, 25.0, 0.0),
-                    child: Column(
-                      children: <Widget>[
-                        TextField(
-                          controller: _titleTextController,
-                          inputFormatters: [
-                            LengthLimitingTextInputFormatter(15)
-                          ],
-                          style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.black.withOpacity(0.80)),
-                          decoration: InputDecoration.collapsed(
-                            hintText: "Give this note a title...",
-                            hintStyle: TextStyle(color: Colors.black45),
-                          ),
-                        ),
-                        Divider(height: 5.0),
-                        SizedBox(height: 10.0),
-                        TextField(
-                          controller: _textController,
-                          maxLines: null,
-                          style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w400,
-                              color: Colors.black.withOpacity(0.80)),
-                          decoration: InputDecoration.collapsed(
-                            hintText: "Write something...",
-                            hintStyle: TextStyle(color: Colors.black45),
-                          ),
-                        ),
-                        SizedBox(height: 200.0)
-                      ],
+        padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+        child: SingleChildScrollView(
+          controller: _scrollController,
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 60.0),
+              Container(
+                color: Colors.amberAccent,
+                margin: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 0.0),
+                padding: EdgeInsets.fromLTRB(25.0, 5.0, 25.0, 0.0),
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      controller: _titleTextController,
+                      inputFormatters: [LengthLimitingTextInputFormatter(15)],
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black.withOpacity(0.80)),
+                      decoration: InputDecoration.collapsed(
+                        hintText: "Give this note a title...",
+                        hintStyle: TextStyle(color: Colors.black45),
+                      ),
                     ),
-                  ),
-                  SizedBox(height: MediaQuery.of(context).size.height)
-                ],
+                    Divider(height: 5.0),
+                    SizedBox(height: 10.0),
+                    TextField(
+                      controller: _textController,
+                      maxLines: null,
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black.withOpacity(0.80)),
+                      decoration: InputDecoration.collapsed(
+                        hintText: "Write something...",
+                        hintStyle: TextStyle(color: Colors.black45),
+                      ),
+                    ),
+                    SizedBox(height: 200.0)
+                  ],
+                ),
               ),
-            ),
-            buildSubTopBar(title = widget.note.title, context),
-          ])),
+              SizedBox(height: MediaQuery.of(context).size.height)
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
